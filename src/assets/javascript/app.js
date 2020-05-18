0cb330cca2126640cf923e0fade50110b0c9605b
Jacob Wilder <jacob.wilder826@gmail.com>
2020-05-18 19:55:51 -0400

    added some styling/ implementing game logic

--- src/assets/javascript/app.js
+++ src/assets/javascript/app.js
@@ -1,12 +1,27 @@
-import questions from './quiz';
+// import questions from './quiz';
 $(document).ready(function () {
     let timeLeft = 15;
-    let countDownl
+    let countDown;
     let correct = 0;
     let incorrect = 0;
     let userChoice = [];
+    let generatedQuiz;
 
+    $("#start").on("click", timer);
 
+    function timer() {
+        clearInterval(countDown);
+        countDown = setInterval(decrement, 1000);
+    }
+
+    function decrement() {
+        timeLeft--;
+        $(".jumbotron").html("<h2>Time Remaining: " + timeLeft + "</h2>");
+        if (timeLeft === 0) {
+            alert("You ran out of time!");
+            stop();
+        }
+    }
 
 })
 
