c3c67ed46bbf127272f3139f5964f18eaf76c721
Jacob Wilder <jacob.wilder826@gmail.com>
2020-05-20 14:26:45 -0400

    added questions/ implemented button styling/ added inline formsto record user answers

--- src/assets/javascript/app.js
+++ src/assets/javascript/app.js
@@ -1,18 +1,548 @@
 // import questions from './quiz';
 $(document).ready(function () {
-    
-    let timeLeft = 16;
+
+    // Object consisting of 50 questions
+    let questions = [{
+            question: "In what year was the first ever Wimbledon Championship held?",
+            answers: [
+                "1877",
+                "1853",
+                "1792",
+                "1926"
+            ],
+            correct: "1877"
+        },
+        {
+            question: "In what year was the first ever Wimbledon Championship held?",
+            answers: [
+                "Iron",
+                "Nickel",
+                "Helium",
+                "Mercury"
+            ],
+            correct: "Mercury"
+        },
+        {
+            question: "Which email service is owned by Microsoft?",
+            answers: [
+                "Gmail",
+                "ProtonMail",
+                "Hotmail",
+                "Thunderbird"
+            ],
+            correct: "Hotmail"
+        },
+        {
+            question: "Which country produces the most coffee in the world?",
+            answers: [
+                "Brazil",
+                "Argentina",
+                "United States",
+                "Puerto Rico"
+            ],
+            correct: "Brazil"
+        },
+        {
+            question: "In which city was Jim Morrison buried?",
+            answers: [
+                "Paris",
+                "Rome",
+                "Sorrento",
+                "Cainnes"
+            ],
+            correct: "Paris"
+        },
+        {
+            question: "What was the first state?",
+            answers: [
+                "New York",
+                "Pennyslvania",
+                "Delaware",
+                "New Jersey"
+            ],
+            correct: "Delaware"
+        },
+        {
+            question: "What is the capital city of Spain?",
+            answers: [
+                "Barcelona",
+                "Salamanca",
+                "Madrid",
+                "Granada"
+            ],
+            correct: "Madrid"
+        },
+        {
+            question: "What is the painting ‘La Gioconda’ more usually known as?",
+            answers: [
+                "The Jovial One",
+                "Mona Lisa",
+                "The Last Supper",
+                "Duality of Man"
+            ],
+            correct: "Mona Lisa"
+        },
+        {
+            question: "What is Chandler’s last name in the sitcom Friends?",
+            answers: [
+                "Bing",
+                "Muriel",
+                "Perry",
+                "Geller"
+            ],
+            correct: "Bing"
+        },
+        {
+            question: "About how many taste buds does the average human tongue have?",
+            answers: [
+                "100,000",
+                "250,000",
+                "10,000",
+                "1+ Million"
+            ],
+            correct: "10,000"
+        },
+        {
+            question: "How much does the Chewbacca costume weigh?",
+            answers: [
+                "10 lbs",
+                "20 lbs",
+                "5 lbs",
+                "8 lbs"
+            ],
+            correct: "8 lbs"
+        },
+        {
+            question: "Where would you find the world’s most ancient forest?",
+            answers: [
+                "Iceland",
+                "Russia",
+                "Japan",
+                "Australia"
+            ],
+            correct: "Australia"
+        },
+        {
+            question: "Globe and Jerusalem are types of what?",
+            answers: [
+                "Plants",
+                "Geographical Features",
+                "Artichoke",
+                "Drugs"
+            ],
+            correct: "Artichoke"
+        },
+        {
+            question: "Which is the highest waterfall in the world?",
+            answers: [
+                "Yosemite Falls",
+                "Tugela Falls",
+                "Niagara Falls",
+                "Angel Falls"
+            ],
+            correct: "Angel Falls"
+        },
+        {
+            question: "Ludwig Van Beethoven was born in 1770 in which city?",
+            answers: [
+                "Berlin",
+                "Teltow",
+                "Vienna",
+                "Venice"
+            ],
+            correct: "Berlin"
+        },
+        {
+            question: "What is the third sign of the Zodiac?",
+            answers: [
+                "Taurus",
+                "Leo",
+                "Gemini",
+                "Virgo"
+            ],
+            correct: "Gemini"
+        },
+        {
+            question: "Who discovered Penicillin?",
+            answers: [
+                "Fleming",
+                "Hoffman",
+                "McKenna",
+                "Freud"
+            ],
+            correct: "Fleming"
+        },
+        {
+            question: "Which name is rapper Sean Combs better known by?",
+            answers: [
+                "50 Cent",
+                "P. Diddy",
+                "Quavo",
+                "Chance the Rapper"
+            ],
+            correct: "P. Diddy"
+        },
+        {
+            question: "Which country invented tea?",
+            answers: [
+                "Kenya",
+                "England",
+                "Japan",
+                "China"
+            ],
+            correct: "China"
+        },
+        {
+            question: "Pure water as a pH level of around?",
+            answers: [
+                "5",
+                "10",
+                "7",
+                "8"
+            ],
+            correct: "7"
+        },
+        {
+            question: "Which is the only vowel on a standard keyboard that is not on the top line of letters?",
+            answers: [
+                "A",
+                "E",
+                "U",
+                "O"
+            ],
+            answers: "A"
+        },
+        {
+            question: "What was Britney Spears’ first song?",
+            answers: [
+                "Womanizer",
+                "Baby One More Time",
+                "Oops...I Did It Again",
+                "Toxic"
+            ],
+            correct: "Baby One More Time"
+        },
+        {
+            question: "What did Queen Anne die of?",
+            answers: [
+                "Poison",
+                "Suicide",
+                "Stroke",
+                "Cancer"
+            ],
+            correct: "Stroke"
+        },
+        {
+            question: "Groups of lions are known as a what?",
+            answers: [
+                "Pride",
+                "Murder",
+                "Hunt",
+                "Squad"
+            ],
+            correct: "Prides"
+        },
+        {
+            question: "How many pairs of wings does a bee have?",
+            answers: [
+                "4",
+                "2",
+                "6",
+                "8"
+            ],
+            correct: "2"
+        },
+        {
+            question: "What language has the most words?",
+            answers: [
+                "French",
+                "Spanish",
+                "Chinese",
+                "English"
+            ],
+            correct: "English"
+        },
+        {
+            question: "What’s the most expensive home in the world?",
+            answers: [
+                "Witanhurst",
+                "Antilia",
+                "White House",
+                "Buckingham Palace"
+            ],
+            correct: "Buckingham Palace"
+        },
+        {
+            question: "Kodiak island is in which US state?",
+            answers: [
+                "Alaska",
+                "Kentucky",
+                "California",
+                "New York"
+            ],
+            correct: "Alaska"
+        },
+        {
+            question: "Which castle is on the island of Anglesey?",
+            answers: [
+                "Sarkova",
+                "Limone",
+                "Saratoga",
+                "Beaumaria"
+            ],
+            correct: "Beaumaria"
+        },
+        {
+            question: "How long does it take to hard boil an egg?",
+            answers: [
+                "7 minutes",
+                "8 minutes",
+                "5 minutes",
+                "90 seconds"
+            ],
+            correct: "7 minutes"
+        },
+        {
+            question: "What nationality was Marco Polo?",
+            answers: [
+                "Italian",
+                "Venetian",
+                "Russian",
+                "Czechoslovakian"
+            ],
+            correct: "Venetian"
+        },
+        {
+            question: "Which scientist was awarded the 1921 Nobel Prize in Physics",
+            answers: [
+                "Freud",
+                "Newton",
+                "Einstein",
+                "Darwin"
+            ],
+            correct: "Einstein"
+        },
+        {
+            question: "Name the world’s largest ocean.",
+            answers: [
+                "Mediterranean",
+                "Pacific",
+                "Indian",
+                "Atlantic"
+            ],
+            correct: "Pacific"
+        },
+        {
+            question: "What did the Crocodile swallow in Peter Pan?",
+            answers: [
+                "Toothbrush",
+                "Soap",
+                "Alarm Clock",
+                "Fish"
+            ],
+            correct: "Alarm Clock"
+        },
+        {
+            question: "What state is the Lincoln family home (Hildene) located in?",
+            answers: [
+                "Vermont",
+                "Kansas",
+                "Pennyslvania",
+                "S. Carolina"
+            ],
+            correct: "Vermont"
+        },
+        {
+            question: "Zurich is the largest city in what country?",
+            answers: [
+                "Ukraine",
+                "Greenland",
+                "Iceland",
+                "Switzerland"
+            ],
+            correct: "Switzerland"
+        },
+        {
+            question: "How many phases of the moon are there?",
+            answers: [
+                "6",
+                "8",
+                "31",
+                "365"
+            ],
+            correct: "8"
+        },
+        {
+            question: "What’s the hardest rock?",
+            answers: [
+                "Amethyst",
+                "Quartz",
+                "Diamond",
+                "Basalt"
+            ],
+            correct: "Diamond"
+        },
+        {
+            question: "How many bones do sharks have in their bodies?",
+            answers: [
+                "More than humans",
+                "Less than humans",
+                "Same as humans",
+                "None"
+            ],
+            correct: "None"
+        },
+        {
+            question: "The fear referred to as arachnophobia indicates a fear of what?",
+            answers: [
+                "Spiders",
+                "Bugs",
+                "Insects",
+                "Loneliness"
+            ],
+            correct: "Spiders"
+        },
+        {
+            question: "What color is a Himalayan poppy flower?",
+            answers: [
+                "White",
+                "Blue",
+                "Red",
+                "Orange"
+            ],
+            correct: "Blue"
+        },
+        {
+            question: "What was the name of the family who starred in 7th Heaven?",
+            answers: [
+                "The Camdens",
+                "The Whoppers",
+                "The Whites",
+                "The Duggars"
+            ],
+            correct: "The Camdens"
+        },
+        {
+            question: "Name the world’s biggest island.",
+            answers: [
+                "Greenland",
+                "Caiman",
+                "Hawaii",
+                "Puerto Rico"
+            ],
+            correct: "Greenland"
+        },
+        {
+            question: "Which sport does Constantino Rocca play?",
+            answers: [
+                "Soccer",
+                "Football",
+                "Golf",
+                "Baseball"
+            ],
+            correct: "Golf"
+        },
+        {
+            question: "What flavor is Cointreau?",
+            answers: [
+                "Orange",
+                "Lemon",
+                "Peach",
+                "Lime"
+            ],
+            correct: "Orange"
+        },
+        {
+            question: "Which country is Prague in?",
+            answers: [
+                "Ukraine",
+                "Czech Republic",
+                "Russia",
+                "Bosnia"
+            ],
+            correct: "Czech Republic"
+        },
+        {
+            question: "What does the term ‘piano’ mean?",
+            answers: [
+                "Powerful",
+                "Calm",
+                "Soft",
+                "Hard"
+            ],
+            correct: "To be played softly"
+        },
+        {
+            question: "Who was the first American to go into space?",
+            answers: [
+                "Buzz Aldrin",
+                "Neil Armstrong",
+                "Alan Shephard",
+                "Michael Collins"
+            ],
+            correct: "Alan Shephard"
+        },
+        {
+            question: "How many hearts does an octopus have?",
+            answers: [
+                "8",
+                "10",
+                "12",
+                "3"
+            ],
+            correct: "Three"
+        }
+    ];
+
+    let timeLeft = 15;
     let countDown;
     let correct = 0;
     let incorrect = 0;
     let userChoice = [];
     let generatedQuiz;
 
+    $("#finish").hide();
+    $(".questions").hide();
+
     $("#start").on("click", timer);
+
+    // adds questions to headers with respective tags
+    $("#question1").prepend(questions[0].question);
+    $("#question2").prepend(questions[1].question);
+    $("#question3").prepend(questions[2].question);
+    $("#question4").prepend(questions[3].question);
+
+    $("#question1-answers input").each(function (i) {
+
+        $(this).next().html(questions[0].answers[i]);
+
+    });
+
+    $("#question2-answers input").each(function (i) {
+
+        $(this).next().html(questions[1].answers[i]);
+
+    });
+
+    $("#question3-answers input").each(function (i) {
+
+        $(this).next().html(questions[2].answers[i]);
+
+    });
+
+    $("#question4-answers input").each(function (i) {
+
+        $(this).next().html(questions[3].answers[i]);
+
+    });
 
     function timer() {
         clearInterval(countDown);
         countDown = setInterval(decrement, 1000);
+        $(".jumbotron").html("<h2>Time Remaining: " + timeLeft + "</h2>");
+        $("#buttons").hide();
+        $("#finish").show();
+        $(".questions").show();
     }
 
     function decrement() {
@@ -24,9 +554,9 @@ $(document).ready(function () {
                 text: 'Click to try again',
                 icon: 'error',
                 confirmButtonText: 'Restart'
-              }).then(function(result) {
-                  window.location = 'index.html';
-              });
+            }).then(function (result) {
+                window.location = 'index.html';
+            });
             stop();
         }
     }
@@ -34,24 +564,23 @@ $(document).ready(function () {
 
     function stop() {
         clearInterval(countDown);
-        
+
         userChoice.push($('input[name="inlineRadioOptions1"]:checked').val());
         userChoice.push($('input[name="inlineRadioOptions2"]:checked').val());
         userChoice.push($('input[name="inlineRadioOptions3"]:checked').val());
-        userChoice.push($('input[name="inlineRadioOptions4"]:checked').val());    
-        
+        userChoice.push($('input[name="inlineRadioOptions4"]:checked').val());
+
         for (i = 0; i < questions.length; i++) {
             if (userChoice[i] === questions[i].correct) {
                 correct++;
             } else {
                 wrong++;
             }
-        }          
-         
+        }
+
         $("#correct").html("<h2>Correct Guesses: " + correct + "</h2>");
         $("#wrong").html("<h2>Wrong Guesses: " + wrong + "</h2>");
         $("#score").html("<h2>Total Score: " + correct + " out of " + (wrong + correct) + "</h2>");
     }
 
 })
-
