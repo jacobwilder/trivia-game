62849dbbcccded1815a296e7e204fdd6a401f668
Jacob Wilder <jacob.wilder826@gmail.com>
2020-05-19 21:01:18 -0400

    implemented stop function/ added sweetalert2

--- src/assets/javascript/app.js
+++ src/assets/javascript/app.js
@@ -1,6 +1,7 @@
 // import questions from './quiz';
 $(document).ready(function () {
-    let timeLeft = 15;
+    
+    let timeLeft = 16;
     let countDown;
     let correct = 0;
     let incorrect = 0;
@@ -18,9 +19,36 @@ $(document).ready(function () {
         timeLeft--;
         $(".jumbotron").html("<h2>Time Remaining: " + timeLeft + "</h2>");
         if (timeLeft === 0) {
-            alert("You ran out of time!");
+            Swal.fire({
+                title: 'You ran out of time!',
+                text: 'Click to try again',
+                icon: 'error',
+                confirmButtonText: 'Restart'
+              });
             stop();
         }
+    }
+
+
+    function stop() {
+        clearInterval(countDown);
+        
+        userChoice.push($('input[name="inlineRadioOptions1"]:checked').val());
+        userChoice.push($('input[name="inlineRadioOptions2"]:checked').val());
+        userChoice.push($('input[name="inlineRadioOptions3"]:checked').val());
+        userChoice.push($('input[name="inlineRadioOptions4"]:checked').val());    
+        
+        for (i = 0; i < questions.length; i++) {
+            if (userChoice[i] === questions[i].correct) {
+                correct++;
+            } else {
+                wrong++;
+            }
+        }          
+         
+        $("#correct").html("<h2>Correct Guesses: " + correct + "</h2>");
+        $("#wrong").html("<h2>Wrong Guesses: " + wrong + "</h2>");
+        $("#score").html("<h2>Total Score: " + correct + " out of " + (wrong + correct) + "</h2>");
     }
 
 })
