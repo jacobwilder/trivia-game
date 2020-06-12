e1b25f38835c18663fe17a8659a5c5e1c6abae20
Jacob Wilder <jacob.wilder826@gmail.com>
2020-06-12 02:00:06 -0400

    tidying up code

--- assets/javascript/animate.js
+++ assets/javascript/animate.js
@@ -1,16 +1,8 @@
 $(document).ready(function () {
+   gsap.from(".jumbotron", {duration: 0.5, y: -300, scale: 1});
+   gsap.from("#quiz-wrapper", {duration: 0.5, y: 800, scale: 1});
 
-
- $("#start").on("click", function() {
-    gsap.to("#quiz-wrapper", {duration: 0.5, rotation: 360, scale: 1})
-
-
- })
-    gsap.from(".jumbotron", {duration: 0.5, y: -300, scale: 1});
-    gsap.from("#quiz-wrapper", {duration: 0.5, y: 800, scale: 1})
-
-
-
-
-
+   $("#start").on("click", function() {
+      gsap.to("#quiz-wrapper", {duration: 0.5, rotation: 360, scale: 1})
+   });
 })
